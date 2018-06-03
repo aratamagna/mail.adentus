b249c7c55d83a925b9e1a70febd882a3e6b2beed
arata <aratamagna@gmail.com>
2018-06-03 16:00:29 -0400

    basicauth namespace

--- src/middleware.php
+++ src/middleware.php
@@ -3,6 +3,8 @@
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
 
+use Tuupola\Middleware\HttpBasicAuthentication;
+
 $app->add(new Tuupola\Middleware\HttpBasicAuthentication([
     "users" => $this->users
     ]));
