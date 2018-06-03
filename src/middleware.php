17e05bdf9c8ee1d4deb6c97d19496d1b106fbd59
arata <aratamagna@gmail.com>
2018-06-03 16:02:42 -0400

    namespace rm

--- src/middleware.php
+++ src/middleware.php
@@ -3,8 +3,6 @@
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
 
-use Tuupola\Middleware\HttpBasicAuthentication;
-
 $app->add(new Tuupola\Middleware\HttpBasicAuthentication([
     "users" => $this->users
     ]));
