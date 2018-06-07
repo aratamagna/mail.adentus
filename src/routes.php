3d8ac783854de276e6670556eef42520e4ff63e9
arata <aratamagna@gmail.com>
2018-06-07 15:16:09 -0400

    attach file

--- src/routes.php
+++ src/routes.php
@@ -14,24 +14,24 @@ $app->post('/', function ($request, $response, $args) {
     $this->mail->setFrom('noreply@adentus.com', 'Contacto');
     $this->mail->addAddress($input['address']);
 
-    if (empty($input['attachment'])){
+    if (!empty($input['attachment'])){
       if (array_key_exists('file', $input['attachment']){
         $attachment = substr($input['attachment']['file'], strpos($input['attachment']['file'], ","));
+        if (!array_key_exists('name', $input['attachment'])){
+          $name = uniqid();
+        } else {
+          $name = $input['attachment']['name'];
+        }
+        if (!array_key_exists('encoding', $input['attachment'])){
+          $encoding = "base64";
+        }
+        if (!array_key_exists('type', $input['attachment'])){
+          $type = mime_content_type($name);
+        } else {
+          $type = $input['attachment']['type'];
+        }
+        $this->mail->addStringAttachment($attachment, $name, $encoding, $type);
       }
-      if (!array_key_exists('name', $input['attachment'])){
-        $name = uniqid();
-      } else {
-        $name = $input['attachment']['name'];
-      }
-      if (!array_key_exists('encoding', $input['attachment'])){
-        $encoding = "base64";
-      }
-      if (!array_key_exists('type', $input['attachment'])){
-        $type = mime_content_type($name);
-      } else {
-        $type = $input['attachment']['type'];
-      }
-      $this->mail->addStringAttachment($attachment, $name, $encoding, $type);
     }
 
     //Content
